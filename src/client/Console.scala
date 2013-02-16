a469cdc9a81a94d43bf24e7348ac9b100277dd79
ShiZhan <g.shizhan.g@gmail.com>
2013-02-17 01:16:59 +0800

    prepare to implement console

--- src/client/Console.scala
+++ src/client/Console.scala
@@ -3,6 +3,8 @@
  */
 package client
 
+import org.apache.commons.cli._
+
 import util.Logging
 
 /**
@@ -13,6 +15,32 @@ object Console extends Logging {
 
 	def run(address: String) {
 		logger.info("Opening CLI on " + address)
+		
+		println("TriGraM shell")
+
+		val parser: CommandLineParser = new PosixParser()
+		val options = new Options()
+    options.addOption("h", "help", false, "print this message" );
+    options.addOption("v", "version", false, "show program version")
+
+		var runningFlag = true
+
+//		do {
+//      try {
+//        // parse the command line arguments
+//        val line: CommandLine = parser.parse(options, System.in.read())
+//  
+//        if(line.hasOption("h")) {
+//        }
+//  
+//        if(line.hasOption("exit")) runningFlag = false
+//
+//      }
+//      catch {
+//        case exp: ParseException =>
+//          logger.warn( "Unrecognized command:" + exp.getMessage())
+//      }
+//		} while (runningFlag)
 	}
 
 }
