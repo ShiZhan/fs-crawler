d5b190485ebef732dfd4e77619bf4ece5354916e
ShiZhan <g.shizhan.g@gmail.com>
2013-03-06 00:36:50 +0800

    loader scripts based on metadata

--- bin/trigram.cmd
+++ bin/trigram.cmd
@@ -1,2 +1,10 @@
-@set WDIR=%~dp0 
-@scala -cp "*.jar;%WDIR%" trigram %1 %2 %3 %4 %5 %6 
+@REM launcher script
+@REM Shi.Zhan @ 2013
+@echo off
+for /f delims^=^"^ tokens^=2 %%i in ('find "lib" ..\.classpath') do call :concat %%i
+scala -cp "%CP%;../target/scala-2.10/classes" trigram %1 %2 %3 %4 %5 %6 
+goto :eof
+
+:concat
+set CP=%CP%%1;
+goto :eof
