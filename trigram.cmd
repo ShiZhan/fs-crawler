1aa99a9e938555e724c92183cc6be95ad0b709c5
ShiZhan <g.shizhan.g@gmail.com>
2013-03-08 10:10:34 +0800

    fix windows batch

--- trigram.cmd
+++ trigram.cmd
@@ -3,9 +3,11 @@
 @REM use .classpath to load dependencies
 @REM use relative directory to load compiled classes
 @echo off
+setlocal
 set CP=
 for /f delims^=^"^ tokens^=2 %%i in ('find "lib" .classpath') do call :concat %%i
 scala -cp "%CP%;target/scala-2.10/classes" trigram %1 %2 %3 %4 %5 %6 
+endlocal
 goto :eof
 
 :concat
