fdcd4df3d174df7f46d9f1f5b8f6ee08a4531557
ShiZhan <g.shizhan.g@gmail.com>
2013-03-20 01:02:34 +0800

    construct sparql test

--- src/test/scala/query.scala
+++ src/test/scala/query.scala
@@ -25,3 +25,20 @@ object QuerySelect {
       store.close
     }
 }
+
+object QueryConstruct {
+  def main(args: Array[String]) =
+    if(args.length < 2)
+      println("run with <dataset> <query>")
+    else {
+      val store = TDBFactory.createDataset(args(0))
+      val queryString = Source.fromFile(args(1)).getLines.mkString("\n")
+      println("Query:\n" + queryString)
+      val query = QueryFactory.create(queryString)
+      val qexec = QueryExecutionFactory.create(query, store)
+      val results = qexec.execConstruct
+      println(results.size)
+      qexec.close
+      store.close
+    }
+}
