1ae45272c4ad6db4b6ccb350a2856647d35e176f
ShiZhan <g.shizhan.g@gmail.com>
2013-04-23 01:34:25 +0800

    class hierarchy translated (again after 1 year ago cim2rdf, 2 year ago XQuery)

--- src/main/scala/modeler/CimModeler.scala
+++ src/main/scala/modeler/CimModeler.scala
@@ -1,5 +1,5 @@
 /**
- *
+ * CIM schema modeler
  */
 package modeler
 
@@ -7,11 +7,11 @@ import scala.xml.XML
 import com.hp.hpl.jena.rdf.model.ModelFactory
 import com.hp.hpl.jena.vocabulary.{ RDF, RDFS, OWL, OWL2, DC_11 => DC, DCTerms => DT }
 import com.hp.hpl.jena.datatypes.xsd.XSDDatatype._
-import util.{ Logging, Version, DateTime, Hash }
+import util.{ Logging, Version, DateTime }
 
 /**
  * @author ShiZhan
- * translate DMTF CIM specification into TriGraM model
+ * translate DMTF CIM schema [http://dmtf.org/standards/cim] into TriGraM model
  */
 object CIM {
 
@@ -19,6 +19,7 @@ object CIM {
   val base = "https://sites.google.com/site/ontology2013/" + local
   val ns = base + "#"
 
+  def ##(name: String) = ns + name
 }
 
 object CimModeler extends Modeler with Logging {
@@ -30,32 +31,70 @@ object CimModeler extends Modeler with Logging {
   def tBox = {
     logger.info("prepare meta-model for CIM schema translation")
 
-    logger.info("Work in Progress")
+    logger.info("TODO: create a meta-model to translate the detail of qualifiers")
   }
 
   def aBox(input: String, output: String) = {
     logger.info("translate CIM schema from [" + input + "] to [" + output + "]")
 
-    val i = XML.loadFile(input).toSeq
-    val classes = i \\ "VALUE.OBJECT" \ "CLASS"
+    val xml = XML.loadFile(input)
+    val classes = xml \\ "VALUE.OBJECT" \ "CLASS"
     val rNodes = classes.flatMap(c => c \ "PROPERTY.REFERENCE")
     val pNodes = classes.flatMap(c => c \ "PROPERTY" ++ c \ "PROPERTY.ARRAY")
-    val rNames = rNodes.map(r => (r \ "@NAME").toString)
-    val pNames = pNodes.map(p => (p \ "@NAME").toString)
-    val objProp = rNames.distinct
-    val datProp = pNames.distinct
+    val objProp = rNodes.map(r => (r \ "@NAME").toString).distinct
+    val datProp = pNodes.map(p => (p \ "@NAME").toString).distinct
 
     logger.info("[%d] classes [%d] object properties [%d] data type properties".
       format(classes.length, objProp.length, datProp.length))
 
-    val o = ModelFactory.createDefaultModel
+    val license = """
+Copyright 2013 Shi.Zhan.
+Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-    if (o.isEmpty)
+    http://www.apache.org/licenses/LICENSE-2.0.
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an &quot;AS IS&quot; BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing
+permissions and limitations under the License. 
+"""
+
+    val m = ModelFactory.createDefaultModel
+    m.setNsPrefix(key, CIM.ns)
+    m.createResource(CIM.base, OWL.Ontology)
+      .addProperty(DC.date, DateTime.get, XSDdateTime)
+      .addProperty(DC.description, "TriGraM model of CIM schema", XSDstring)
+      .addProperty(DT.license, license, XSDstring)
+      .addProperty(OWL.versionInfo, Version.get, XSDstring)
+
+    val cMeta = m.createResource(CIM ## "CIM_Meta_Class", OWL.Class)
+    val cAsso = m.createResource(CIM ## "CIM_Association", OWL.Class)
+      .addProperty(RDFS.subClassOf, cMeta)
+
+    for (c <- classes) {
+      val cName = (c \ "@NAME").toString
+      val cSuperName = (c \ "@SUPERCLASS").toString
+      val cQualifier = c \ "QUALIFIER"
+      val cIsAsso = cQualifier.map(q => (q \ "@NAME").toString).contains("Association")
+
+      val cSuper = if (cSuperName.isEmpty)
+        if (cIsAsso) cAsso else cMeta
+      else
+        m.getResource(CIM ## cSuperName)
+
+      val cClass = m.createResource(CIM ## cName, OWL.Class)
+        .addProperty(RDFS.subClassOf, cSuper)
+    }
+
+    if (m.isEmpty)
       logger.info("Nothing translated")
     else {
-      o.write(new java.io.FileOutputStream(output), "RDF/XML-ABBREV")
+      m.write(new java.io.FileOutputStream(output), "RDF/XML-ABBREV")
 
-      logger.info("[%d] triples generated".format(o.size))
+      logger.info("[%d] triples generated".format(m.size))
     }
   }
 
--- src/test/scala/XML.scala
+++ src/test/scala/XML.scala
@@ -7,13 +7,13 @@
  */
 import scala.xml.XML
 
-object CountNode {
+object CountNodes {
 
   def main(args: Array[String]) =
     if (args.length < 1)
       println("run with <XML file>")
     else {
-      val i = XML.loadFile(args(0)).toSeq
+      val i = XML.loadFile(args(0))
       val classes = i \\ "VALUE.OBJECT" \ "CLASS"
       val references = classes.flatMap(c => c \ "PROPERTY.REFERENCE")
       val properties = classes.flatMap(c => c \ "PROPERTY" ++ c \ "PROPERTY.ARRAY")
@@ -29,4 +29,26 @@ object CountNode {
         format(references.length, objProp.length, properties.length, datProp.length))
     }
 
+}
+
+object TravelClasses {
+
+  def main(args: Array[String]) =
+    if (args.length < 1)
+      println("run with <XML file>")
+    else {
+      val i = XML.loadFile(args(0))
+      val classes = i \\ "VALUE.OBJECT" \ "CLASS"
+      for(c <- classes){
+        val cName = (c \ "@NAME").toString
+        val cSuperName = (c \ "@SUPERCLASS").toString
+        val cQualifier = c \ "QUALIFIER"
+        val cIsAsso = cQualifier.map(q => (q \ "@NAME").toString).contains("Association")
+        if (cSuperName.isEmpty)
+          println(cName + " : " + cIsAsso + " has no super class")
+        else
+          println(cName + " : " + cIsAsso + "|" + cSuperName)
+      }
+    }
+  
 }
