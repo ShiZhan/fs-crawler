2f1e6a3a889a13536fb49d419ce452ed08a7bcf4
ShiZhan <g.shizhan.g@gmail.com>
2013-04-25 13:46:02 +0800

    test query on large dataset

--- trigram.cmd
+++ trigram.cmd
@@ -4,6 +4,9 @@
 @REM use absolute directory to load compiled classes
 @echo off
 setlocal
+@REM for querying big models
+set JAVA_OPTS=%JAVA_OPTS% -Xms1024m -Xmx1024m
+
 set CP=
 set TGM_ROOT=%~dp0
 for /f delims^=^"^ tokens^=2 %%i in ('find "lib" "%TGM_ROOT%.classpath"') do call :concat %%i
