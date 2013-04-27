e05c97dbc9bb9c3fc0c41eaa8d6fb52f95261b6d
ShiZhan <g.shizhan.g@gmail.com>
2013-04-27 17:05:23 +0800

    use DSL to create tgmdir model

--- src/test/scala/DSLModel.scala
+++ src/test/scala/DSLModel.scala
@@ -19,9 +19,8 @@ object DSLModel {
   def main(args: Array[String]): Unit = {
     val model = ModelFactory.createDefaultModel
     val m = DSLElement(model)
-    val base = "http://localhost/test"
 
-    ((m <-- (base, OWL.Ontology))
+    ((m <-- (DIR.base, OWL.Ontology))
       -- DC.date --> (DateTime.get, XSDdateTime)
       -- DC.description --> ("test model", XSDstring)
       -- DT.license --> ("Apache 2", XSDstring)
@@ -37,17 +36,60 @@ object DSLModel {
 
     m <-- (DIR.contain.getURI, OWL.ObjectProperty)
 
-    ((m <-- (DIR.Object.getURI, OWL.Class))
-      -- RDFS.subClassOf --> ((m <-- OWL.Restriction)
-        -- OWL.onProperty --> DIR.name
-        -- OWL2.cardinality --> ("1", XSDnonNegativeInteger)
-        -- OWL2.onDataRange --> modeler.XSD.normalizedString)
-      -- RDFS.subClassOf --> ((m <-- OWL.Restriction)
-        -- OWL.onProperty --> DIR.size
-        -- OWL2.cardinality --> ("1", XSDnonNegativeInteger)
-        -- OWL2.onDataRange --> modeler.XSD.unsignedLong))
+    (
+      (m <-- (DIR.Object.getURI, OWL.Class))
+        -- RDFS.subClassOf --> (
+          (m <-- OWL.Restriction)
+            -- OWL.onProperty --> DIR.name
+            -- OWL2.cardinality --> ("1", XSDnonNegativeInteger)
+            -- OWL2.onDataRange --> modeler.XSD.normalizedString
+        )
+        -- RDFS.subClassOf --> (
+          (m <-- OWL.Restriction)
+            -- OWL.onProperty --> DIR.size
+            -- OWL2.cardinality --> ("1", XSDnonNegativeInteger)
+            -- OWL2.onDataRange --> modeler.XSD.unsignedLong
+        )
+        -- RDFS.subClassOf --> (
+          (m <-- OWL.Restriction)
+            -- OWL.onProperty --> DIR.lastModified
+            -- OWL2.cardinality --> ("1", XSDnonNegativeInteger)
+            -- OWL2.onDataRange --> modeler.XSD.dateTime
+        )
+        -- RDFS.subClassOf --> (
+          (m <-- OWL.Restriction)
+            -- OWL.onProperty --> DIR.canRead
+            -- OWL2.cardinality --> ("1", XSDnonNegativeInteger)
+            -- OWL2.onDataRange --> modeler.XSD.xboolean
+        )
+        -- RDFS.subClassOf --> (
+          (m <-- OWL.Restriction)
+            -- OWL.onProperty --> DIR.canWrite
+            -- OWL2.cardinality --> ("1", XSDnonNegativeInteger)
+            -- OWL2.onDataRange --> modeler.XSD.xboolean
+        )
+        -- RDFS.subClassOf --> (
+          (m <-- OWL.Restriction)
+            -- OWL.onProperty --> DIR.canExecute
+            -- OWL2.cardinality --> ("1", XSDnonNegativeInteger)
+            -- OWL2.onDataRange --> modeler.XSD.xboolean
+        )
+        -- RDFS.subClassOf --> (
+          (m <-- OWL.Restriction)
+            -- OWL.onProperty --> DIR.isDirectory
+            -- OWL2.cardinality --> ("1", XSDnonNegativeInteger)
+            -- OWL2.onDataRange --> modeler.XSD.xboolean
+        )
+        -- RDFS.subClassOf --> (
+          (m <-- OWL.Restriction)
+            -- OWL.onProperty --> DIR.contain
+            -- OWL.allValuesFrom --> DIR.Object
+        )
+    )
 
     println(m)
+
+//    m.get.write(new java.io.FileOutputStream(DIR.local), "RDF/XML-ABBREV")
   }
 
 }
