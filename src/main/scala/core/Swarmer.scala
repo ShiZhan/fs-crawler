400810e27146abace7f6729abdd00f7258c12c33
ShiZhan <g.shizhan.g@gmail.com>
2013-04-28 18:55:23 +0800

    prepare to build swarmer module

--- src/main/scala/core/Swarmer.scala
+++ src/main/scala/core/Swarmer.scala
@@ -3,11 +3,16 @@
  */
 package core
 
+import util.Logging
+
 /**
  * @author ShiZhan
  * collaborate with other TriGraM nodes as model implies
  * 1. query remote TDB if local triple refer to remote URI
  * 2. start TDB server over HTTP (maybe Jena Fuseki)
  */
-object Swarmer {
+object Swarmer extends Logging{
+
+  logger.info("TODO: make storage query and update distributely")
+
 }
