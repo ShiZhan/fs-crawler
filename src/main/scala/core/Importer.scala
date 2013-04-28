5dff2fb0946a9a714a0003c1c7cc94894f67d4ae
ShiZhan <g.shizhan.g@gmail.com>
2013-04-28 18:39:51 +0800

    invoke tdb.loader directly

--- src/main/scala/core/Importer.scala
+++ src/main/scala/core/Importer.scala
@@ -3,29 +3,14 @@
  */
 package core
 
-import util.Logging
-
 /**
  * @author ShiZhan
  * 2013
  * import meta-data from model file to TDB data set
  */
-object Importer extends Store(Store.defaultLocation) with Logging {
+//object Importer extends Store(Store.defaultLocation) with Logging {
+object Importer {
 
-  def load(name: String) = {
-    logger.info("importing RDF/OWL model")
-
-    val sparql = "LOAD <%s>".format(name)
-
-    try {
-      sparqlUpdateTxn(sparql)
-    } catch {
-      case e: Exception => println(e)
-    }
-
-    logger.info("done")
-
-    close
-  }
+  def load(name: String) = tdb.tdbloader.main("--loc=" + Store.defaultLocation, name)
 
 }
