687fb4669c475071b8d4ec93ef75df2b94ef5237
ShiZhan <g.shizhan.g@gmail.com>
2013-05-21 11:10:33 +0800

    thinker continues

---
+++ src/main/scala/thinker/Thinker.scala
@@ -0,0 +1,15 @@
+/**
+ * Thinker
+ */
+package thinker
+
+/**
+ * @author ShiZhan
+ * thinker
+ * input 1: base model obtained from TDB query or local RDF model
+ * input 2: problem model
+ * output: result model
+ */
+object Thinker {
+
+}
