341dfdee5d1da23d8d0949fda85ec8e60ec95bb5
ShiZhan <g.shizhan.g@gmail.com>
2013-05-27 10:41:44 +0800

    how to put time-stamp into logger?

--- src/main/scala/util/Loggable.scala
+++ src/main/scala/util/Loggable.scala
@@ -3,12 +3,13 @@
  */
 package util
 
-import org.slf4j.{ Logger, LoggerFactory }
+import org.slf4j.LoggerFactory
 
 /**
  * @author ShiZhan
  * for use slf4j with object
  * Note: implementation taken from scalax.logging API
+ * https://github.com/eengbrec/Scalax.IO/blob/master/src/scalax/logging/log.scala
  */
 trait Logging {
   val logger = Logging.getLogger(this)
