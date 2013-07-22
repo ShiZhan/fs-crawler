ddd8477cf196636c3c86a8247e1b247257ebacb7
ShiZhan <g.shizhan.g@gmail.com>
2013-07-22 15:43:02 -0500

    testing environment builder

---
+++ test/test.bat
@@ -0,0 +1,17 @@
+@echo off
+@echo automatic build up a simple test environment in %~dp0test
+
+setlocal
+
+set TEST_FILE_SOURCE=%~f0
+md test
+cd test
+copy %TEST_FILE_SOURCE% test_file > nul
+copy test_file test_file_readonly > nul
+attrib +r test_file_readonly
+FOR /L %%G IN (1,1,10) DO (
+	md dir%%G
+	copy test_file dir%%G > nul
+)
+
+endlocal
