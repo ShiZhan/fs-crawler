d5f51995857df916f992cf5b7ea67f8ce6d618d3
ShiZhan <g.shizhan.g@gmail.com>
2013-08-07 14:45:17 -0500

    update script to cope with sbteclipse upgrading

--- translator.cmd
+++ translator.cmd
@@ -9,7 +9,7 @@ set JAVA_OPTS=%JAVA_OPTS% -Xms1024m -Xmx1024m
 
 set CP=
 set TGM_ROOT=%~dp0
-for /f delims^=^"^ tokens^=2 %%i in ('find "lib" "%TGM_ROOT%.classpath"') do call :concat %%i
+for /f delims^=^"^ tokens^=4 %%i in ('find "lib" "%TGM_ROOT%.classpath"') do call :concat %%i
 scala -cp "%CP%;%TGM_ROOT%;%TGM_ROOT%target/scala-2.10/classes" TrigramTranslator %1 %2 %3 %4 %5 %6 
 endlocal
 goto :eof
