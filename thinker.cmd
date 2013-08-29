6e8ea0eadeea5a70b11f8c36517b0fac7a2f84b2
ShiZhan <g.shizhan.g@gmail.com>
2013-08-28 23:41:10 -0500

    update script to use collected lib

--- thinker.cmd
+++ thinker.cmd
@@ -9,11 +9,11 @@ set JAVA_OPTS=%JAVA_OPTS% -Xmx1g
 
 set CP=
 set TGM_ROOT=%~dp0
-for /f delims^=^"^ tokens^=4 %%i in ('find "lib" "%TGM_ROOT%.classpath"') do call :concat %%i
+for /f %%i in ('dir /b %TGM_ROOT%target\scala-2.10\lib') do call :concat %%i
 scala -cp "%CP%;%TGM_ROOT%;%TGM_ROOT%target/scala-2.10/classes" TrigramThinker %1 %2 %3 %4 %5 %6 
 endlocal
 goto :eof
 
 :concat
-set CP=%CP%%1;
+set CP=%CP%%TGM_ROOT%target/scala-2.10/lib/%1;
 goto :eof
--- translator.cmd
+++ translator.cmd
@@ -1,19 +1,18 @@
 @REM launcher script
 @REM Shi.Zhan @ 2013
-@REM use .classpath to load dependencies
+@REM use 'sbt copy-dependencies' to collect dependencies
 @REM use absolute directory to load compiled classes
 @echo off
 setlocal
 @REM for translating big in-memory models
 set JAVA_OPTS=%JAVA_OPTS% -Xmx1g
-
 set CP=
 set TGM_ROOT=%~dp0
-for /f delims^=^"^ tokens^=4 %%i in ('find "lib" "%TGM_ROOT%.classpath"') do call :concat %%i
+for /f %%i in ('dir /b %TGM_ROOT%target\scala-2.10\lib') do call :concat %%i
 scala -cp "%CP%;%TGM_ROOT%;%TGM_ROOT%target/scala-2.10/classes" TrigramTranslator %1 %2 %3 %4 %5 %6 
 endlocal
 goto :eof
 
 :concat
-set CP=%CP%%1;
+set CP=%CP%%TGM_ROOT%target/scala-2.10/lib/%1;
 goto :eof
