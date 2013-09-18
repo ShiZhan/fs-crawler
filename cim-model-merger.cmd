377460ca67c7c6f0d052e4fc46c4053bc944b9b3
ShiZhan <g.shizhan.g@gmail.com>
2013-09-18 17:15:50 -0500

    prepare the CIM sub-model merger for CimSchemaEx based model

---
+++ cim-model-merger.cmd
@@ -0,0 +1,18 @@
+@REM launcher script
+@REM Shi.Zhan @ 2013
+@REM use 'sbt copy-dependencies' to collect dependencies
+@REM use absolute directory to load compiled classes
+@echo off
+setlocal
+@REM for translating big in-memory models
+set JAVA_OPTS=%JAVA_OPTS% -Xmx1g
+set CP=
+set TGM_ROOT=%~dp0
+for /f %%i in ('dir /b %TGM_ROOT%target\scala-2.10\lib') do call :concat %%i
+scala -cp "%CP%;%TGM_ROOT%;%TGM_ROOT%target/scala-2.10/classes" CimModelMerger %1 %2 %3 %4 %5 %6 
+endlocal
+goto :eof
+
+:concat
+set CP=%CP%%TGM_ROOT%target/scala-2.10/lib/%1;
+goto :eof
