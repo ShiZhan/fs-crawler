d850a9bab66b47f2cce99490d98263f61ecd7755
ShiZhan <g.shizhan.g@gmail.com>
2013-09-25 13:56:49 -0500

    change test preparation to scala script

---
+++ test/PrepareTestDirectory.scala
@@ -0,0 +1,22 @@
+object PrepareTestDirectory {
+  import java.io.File
+  import java.io.PrintWriter
+
+  def main(args: Array[String]) = {
+    if (args.length < 1)
+      println("usage: PrepareTestDirectory <target directory>")
+    else {
+      val root = args(0)+ "/test"
+      val test = new File(root)
+      test.mkdirs
+      (1 to 10) map {i => new File(root + "/dir" + "%03d".format(i))} foreach {_.mkdir}
+      val genericFile = new PrintWriter(root + "/generic-file")
+      genericFile.println("This is a genereic file.")
+      val readOnlyFile = new File(root + "/read-only-file")
+      val readOnlyFileWriter = new PrintWriter(readOnlyFile)
+      readOnlyFileWriter.println("This is a read only file.")
+      readOnlyFile.setReadOnly
+      println("test directory ready")
+    }
+  }
+}
