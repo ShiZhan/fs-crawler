b9cd0961f105be6b518c3747d29c804fcfb2ac78
ShiZhan <g.shizhan.g@gmail.com>
2013-09-25 14:03:58 -0500

    fix testing script

--- test/PrepareTestDirectory.scala
+++ test/PrepareTestDirectory.scala
@@ -9,13 +9,19 @@ object PrepareTestDirectory {
       val root = args(0)+ "/test"
       val test = new File(root)
       test.mkdirs
+
       (1 to 10) map {i => new File(root + "/dir" + "%03d".format(i))} foreach {_.mkdir}
+
       val genericFile = new PrintWriter(root + "/generic-file")
       genericFile.println("This is a genereic file.")
+      genericFile.close
+
       val readOnlyFile = new File(root + "/read-only-file")
       val readOnlyFileWriter = new PrintWriter(readOnlyFile)
       readOnlyFileWriter.println("This is a read only file.")
+      readOnlyFileWriter.close
       readOnlyFile.setReadOnly
+
       println("test directory ready")
     }
   }
