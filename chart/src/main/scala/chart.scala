4e09824c2f21986eb23de5e08bc2e09f401e6fee
ShiZhan <g.shizhan.g@gmail.com>
2013-10-30 17:06:23 -0500

    bar chart

--- chart/src/main/scala/chart.scala
+++ chart/src/main/scala/chart.scala
@@ -1,4 +1,4 @@
-object chart {
+object xylinechart {
   import scalax.chart.Charting._
   import org.jfree.chart.StandardChartTheme
 
@@ -7,6 +7,25 @@ object chart {
     val data = Seq((1, 2), (2, 4), (3, 6), (4, 8), (5, 10))
     val dataset = data.toXYSeriesCollection("some points")
     val lineChart = XYLineChart(
+      dataset,
+      title = "My Chart of Some Points",
+      domainAxisLabel = "X",
+      rangeAxisLabel = "Y")
+    lineChart.backgroundPaint = new Color(255, 255, 255)
+    lineChart.show
+    lineChart.saveAsPDF("r:/chart.pdf", (1024,768))
+  }
+}
+
+object barchart {
+  import scalax.chart.Charting._
+  import org.jfree.chart.StandardChartTheme
+
+  def main(args: Array[String]) = {
+    implicit val theme = StandardChartTheme.createLegacyTheme
+    val data = Seq((1, 2), (2, 4), (3, 6), (4, 8), (5, 10))
+    val dataset = data.toCategoryDataset
+    val lineChart = BarChart(
       dataset,
       title = "My Chart of Some Points",
       domainAxisLabel = "X",
