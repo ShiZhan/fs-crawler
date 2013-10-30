6966bcc968f544ae4fa93ee0187520d665e35e26
ShiZhan <g.shizhan.g@gmail.com>
2013-10-30 14:37:39 -0500

    this one works

--- chart/build.sbt
+++ chart/build.sbt
@@ -5,5 +5,5 @@ version := "1.0"
 scalaVersion := "2.10.3"
 
 libraryDependencies ++= Seq(
-  "org.jfree" % "jfreechart" % "1.0.15"
+  "com.github.wookietreiber" %% "scala-chart" % "latest.integration"
 )
--- chart/chart.scala
+++ chart/chart.scala
@@ -1,35 +1,14 @@
 object chart {
-  import org.jfree.chart.ChartFactory
-  import org.jfree.chart.ChartUtilities
-  import org.jfree.chart.JFreeChart
-  import org.jfree.data.general._
-  import org.jfree.data._
-  import org.jfree.chart.ChartPanel
-  import org.jfree.chart.plot.PiePlot
-
-  import javax.swing.JFrame
-  import javax.swing.JPanel
+  import scalax.chart._
+  import scalax.chart.Charting._
 
   def main(args: Array[String]) = {
-    val pieDataset = new DefaultPieDataset
-    pieDataset.setValue("A", 75)
-    pieDataset.setValue("B", 10)
-    pieDataset.setValue("C", 10)
-    pieDataset.setValue("D", 5)
-    val chart = ChartFactory.createPieChart(
-      "Hello World",
-      pieDataset,
-      true,
-      true,
-      false)
-
-    println("Hello world")
-
-    val frame = new JFrame("Hello Pie World")
-    frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE )
-    frame.setSize(640, 420)
-    frame.add( new ChartPanel(chart) )
-    frame.pack()
-    frame.setVisible(true)
+    val data    = Seq((1,2),(2,4),(3,6),(4,8))
+    val dataset = data.toXYSeriesCollection("some points")
+    val chart = XYLineChart(dataset, title = "My Chart of Some Points")
+    chart.show
+//    chart.saveAsPNG(new java.io.File("/tmp/chart.png"), (1024,768))
+//    chart.saveAsJPEG(new java.io.File("/tmp/chart.jpg"), (1024,768))
+    chart.saveAsPDF("chart.pdf", (1024,768))
   }
 }
