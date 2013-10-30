e39959a079f3145c0cd68b8bfce1080e1c19b2c7
ShiZhan <g.shizhan.g@gmail.com>
2013-10-30 16:48:10 -0500

    looks OK now

--- chart/src/main/scala/chart.scala
+++ chart/src/main/scala/chart.scala
@@ -1,17 +1,18 @@
 object chart {
-  import scalax.chart._
   import scalax.chart.Charting._
+  import org.jfree.chart.StandardChartTheme
 
   def main(args: Array[String]) = {
+    implicit val theme = StandardChartTheme.createLegacyTheme
     val data = Seq((1, 2), (2, 4), (3, 6), (4, 8), (5, 10))
     val dataset = data.toXYSeriesCollection("some points")
     val lineChart = XYLineChart(
       dataset,
       title = "My Chart of Some Points",
-      legend = true)
-    val color = new Color(255, 255, 255)
-    lineChart.backgroundPaint = color
+      domainAxisLabel = "X",
+      rangeAxisLabel = "Y")
+    lineChart.backgroundPaint = new Color(255, 255, 255)
     lineChart.show
-    //    lineChart.saveAsPDF("r:/chart.pdf", (1024,768))
+    lineChart.saveAsPDF("r:/chart.pdf", (1024,768))
   }
 }
