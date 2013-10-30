b08ed41d4f3f6dcac0a24543c3493aa96e551a06
ShiZhan <g.shizhan.g@gmail.com>
2013-10-30 15:37:41 -0500

    cannot change chart theme?

--- chart/src/main/scala/chart.scala
+++ chart/src/main/scala/chart.scala
@@ -3,10 +3,15 @@ object chart {
   import scalax.chart.Charting._
 
   def main(args: Array[String]) = {
-    val data = Seq((1,2),(2,4),(3,6),(4,8))
+    val data = Seq((1, 2), (2, 4), (3, 6), (4, 8), (5, 10))
     val dataset = data.toXYSeriesCollection("some points")
-    val lineChart = XYLineChart(dataset, title = "My Chart of Some Points")
+    val lineChart = XYLineChart(
+      dataset,
+      title = "My Chart of Some Points",
+      legend = true)
+    val color = new Color(255, 255, 255)
+    lineChart.backgroundPaint = color
     lineChart.show
-    lineChart.saveAsPDF("r:/chart.pdf", (1024,768))
+    //    lineChart.saveAsPDF("r:/chart.pdf", (1024,768))
   }
 }
