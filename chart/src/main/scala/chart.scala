b35acf66f36ee4d128674eff9514676496329afb
ShiZhan <g.shizhan.g@gmail.com>
2013-10-31 03:50:20 -0500

    go sleep

--- chart/src/main/scala/chart.scala
+++ chart/src/main/scala/chart.scala
@@ -1,48 +1,56 @@
 object xylinechart {
   import scalax.chart.Charting._
-  import org.jfree.chart.StandardChartTheme
   import org.jfree.ui.RectangleEdge
-  import java.awt.{ Font, Color }
+  import java.awt.Color
 
   def main(args: Array[String]) = {
-    implicit val theme = new StandardChartTheme("generic")
-    theme.setExtraLargeFont(new Font("Courier New", Font.PLAIN, 20))
-    theme.setLargeFont(new Font("Courier New", Font.PLAIN, 14))
-    theme.setRegularFont(new Font("Courier New", Font.PLAIN, 12))
-    theme.setSmallFont(new Font("Courier New", Font.PLAIN, 10))
-    theme.setPlotBackgroundPaint(Color.white)
-    theme.setDomainGridlinePaint(Color.blue)
-    theme.setRangeGridlinePaint(Color.blue)
-
     val data = Seq((1, 2), (2, 4), (3, 6), (4, 8), (5, 10))
     val dataset = data.toXYSeriesCollection("some points")
-    val lineChart = XYLineChart(
+    val lChart = XYLineChart(
       dataset,
       title = "Chart",
       domainAxisLabel = "X",
       rangeAxisLabel = "Y")
-    lineChart.backgroundPaint = Color.white
-    lineChart.peer.getLegend.setPosition(RectangleEdge.RIGHT)
-    lineChart.show
-    lineChart.saveAsPDF("r:/chart.pdf", (800, 600))
+    val jfc = lChart.peer
+    jfc.getLegend.setPosition(RectangleEdge.RIGHT)
+    jfc.backgroundPaint = Color.white
+    val plot = lChart.plot
+    plot.setBackgroundPaint(Color.white)
+    plot.setDomainGridlinePaint(Color.blue)
+    plot.setRangeGridlinePaint(Color.blue)
+    lChart.show
+    lChart.saveAsPDF("r:/chart.pdf", (800, 600))
   }
 }
 
 object barchart {
   import scalax.chart.Charting._
-  import org.jfree.chart.StandardChartTheme
+  import org.jfree.ui.RectangleEdge
+  import java.awt.{ Color, GradientPaint }
 
   def main(args: Array[String]) = {
-    implicit val theme = StandardChartTheme.createLegacyTheme
-    val data = Seq((1, 2), (2, 4), (3, 6), (4, 8), (5, 10))
+    val data = Seq((1, 4), (2, 4), (3, 6), (4, 8), (5, 10))
     val dataset = data.toCategoryDataset
-    val lineChart = BarChart(
+    val bChart = BarChart(
       dataset,
       title = "My Chart of Some Points",
       domainAxisLabel = "X",
       rangeAxisLabel = "Y")
-    lineChart.backgroundPaint = new Color(255, 255, 255)
-    lineChart.show
-    lineChart.saveAsPDF("r:/chart.pdf", (1024, 768))
+    val jfc = bChart.peer
+    jfc.getLegend.setPosition(RectangleEdge.RIGHT)
+    jfc.backgroundPaint = Color.white
+    val plot = bChart.plot
+    plot.setBackgroundPaint(Color.white)
+    plot.setRangeGridlinePaint(Color.blue)
+    val gp0 = new GradientPaint(0.0f, 0.0f, Color.blue,
+      0.0f, 0.0f, new Color(0, 0, 64))
+    val render = plot.getRenderer
+    render.setSeriesPaint(0, gp0)
+    render.setSeriesPaint(1, gp0)
+    render.setSeriesPaint(2, gp0)
+    render.setSeriesPaint(3, gp0)
+    render.setSeriesPaint(4, gp0)
+    bChart.show
+    bChart.saveAsPDF("r:/chart.pdf", (800, 600))
   }
 }
