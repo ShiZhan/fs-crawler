509270662780396d4b123bb626b799fa4a613348
ShiZhan <g.shizhan.g@gmail.com>
2013-11-02 03:17:38 -0500

    6 fill patterns

--- chart/src/main/scala/util/pattern.scala
+++ chart/src/main/scala/util/pattern.scala
@@ -6,33 +6,31 @@ package util
 /**
  * @author ShiZhan
  * available patterns:
- * 0: '\'
- * 1: '-'
- * 2: '/'
- * 3: '|'
+ * 0: '\', 1: '_', 2: '/', 3: '|', 4: '.', 5: '-'
  */
 object pattern {
-  import java.awt.{Color, Rectangle, TexturePaint}
+  import java.awt.{ Color, Rectangle, TexturePaint }
   import java.awt.image.BufferedImage
   import org.jfree.chart.renderer.category.{ BarRenderer, StandardBarPainter }
 
-  val Fill = List((0, 0, 5, 5), (0, 0, 5, 0), (0, 5, 5, 0), (0, 0, 0, 5)) map {
-    case (x1, y1, x2, y2) => {
-      val bufferedImage = new BufferedImage(5, 5, BufferedImage.TYPE_BYTE_GRAY)
-      val big = bufferedImage.createGraphics
-      big.setColor(Color.white)
-      big.fillRect(0, 0, 5, 5)
-      big.setColor(Color.black)
-      big.drawLine(x1, y1, x2, y2)
-      val imageRect = new Rectangle(0, 0, 5, 5 )
-      new TexturePaint(bufferedImage, imageRect)
-    }
-  } toArray
+  val Fill = List(
+    (0, 0, 5, 5), (0, 0, 5, 0), (0, 5, 5, 0),
+    (0, 0, 0, 5), (0, 0, 0, 0), (1, 1, 3, 3)) map {
+      case (x1, y1, x2, y2) => {
+        val bufferedImage = new BufferedImage(5, 5, BufferedImage.TYPE_BYTE_GRAY)
+        val big = bufferedImage.createGraphics
+        big.setColor(Color.white)
+        big.fillRect(0, 0, 5, 5)
+        big.setColor(Color.black)
+        big.drawLine(x1, y1, x2, y2)
+        val imageRect = new Rectangle(0, 0, 5, 5)
+        new TexturePaint(bufferedImage, imageRect)
+      }
+    } toArray
 
   val Renderer = new BarRenderer
   Renderer.setBarPainter(new StandardBarPainter)
   Renderer.setShadowVisible(false)
   Renderer.setDrawBarOutline(true)
-  (0 to 2) foreach (i => Renderer.setSeriesPaint(i, Fill(i)))
-
+  (0 to Fill.length - 1) foreach (i => Renderer.setSeriesPaint(i, Fill(i)))
 }
