79e2864514b486010d3db60ea508d2b02b156dea
ShiZhan <g.shizhan.g@gmail.com>
2013-11-09 03:32:14 -0600

    introduction on new fromString API

--- src/main/scala/util/URI.scala
+++ src/main/scala/util/URI.scala
@@ -13,6 +13,7 @@ import scalax.file.Path
  * 1. Generate computer node URI from hostname
  * 2. Generate file URI from File instance with hostname
  * 3. Generate path URI from Path instance with hostname
+ * 4. Generate URI from plain String with hostname
  */
 object URI {
   def fromHost = "file:/" + Platform.hostname
