b3f84e9447bb17139e0dec69118b6a71e7491619
ShiZhan <g.shizhan.g@gmail.com>
2013-11-09 01:11:50 -0600

    CSV schema simplified because CIM vocabulary contains necessary definitions.

--- src/main/scala/modeler/CSV.scala
+++ src/main/scala/modeler/CSV.scala
@@ -9,7 +9,7 @@ import au.com.bytecode.opencsv.CSVReader
 import com.hp.hpl.jena.rdf.model.ModelFactory
 import com.hp.hpl.jena.vocabulary.{ RDF, RDFS, OWL, OWL2, DC_11 => DC, DCTerms => DT }
 import com.hp.hpl.jena.datatypes.xsd.XSDDatatype._
-import util.{ Logging, Version, DateTime, Hash }
+import util.{ Logging, Version, DateTime, URI }
 
 import modeler.{ CimVocabulary => CIM }
 
@@ -28,40 +28,48 @@ object CSV extends Modeler with Logging {
       case data :: schema :: tail => translate(data, schema)
       case _ => {
         logger.error("parameter error: [{}]", options)
-        logger.error("must provide a data source CSV and a schema CSV (delimiter: '*').")
+        logger.error("must provide a data source CSV (delimiter: '*') and a schema.")
         logger.error("data CSV:")
         logger.error("column 0:   [individual IDs]")
         logger.error("column 1~m: [property values]")
-        logger.error("schema CSV:")
-        logger.error("line 0:     [Concept URI,  import URI]")
-        logger.error("line 1~m:   [Property URI, import URI]")
+        logger.error("schema:")
+        logger.error("line 0:     [CIM Class]")
+        logger.error("line 1~m:   [CIM Property]")
       }
     }
   }
 
   def loadSchema(schema: String) = {
-    val reader = new CSVReader(new FileReader(schema), '*')
-    val line = reader.readAll.toList map { case Array(u, i) => (u, i) }
-    val concept = line.head._1
-    val properties = line.drop(1).map(_._1)
-    val imports = line.map(_._2).distinct
-    reader.close
-    (concept, properties, imports)
+    val s = io.Source.fromFile(schema)
+    val lines = s.getLines.toList
+    val concept = lines.head
+    val properties = lines.drop(1)
+    s.close
+    (concept, properties)
+  }
+
+  private def initModel(i: String) = {
+    val base = URI.fromHost
+    val ns = base + "#"
+    val m = ModelFactory.createDefaultModel
+    m.setNsPrefix(key, ns)
+    m.setNsPrefix(CimSchema.key, CIM.NS)
+    m.createResource(base, OWL.Ontology)
+      .addProperty(DC.date, DateTime.get, XSDdateTime)
+      .addProperty(DC.description, "TriGraM CSV model", XSDstring)
+      .addProperty(OWL.versionInfo, Version.get, XSDstring)
+      .addProperty(OWL.imports, CIM.IMPORT(i))
+    m
   }
 
   def translate(data: String, schema: String) = {
-    val (c, p, i) = loadSchema(schema)
-    println("Concept: " + c)
-    println("Properties:")
-    p.foreach(println)
-    println("Imports:")
-    i.foreach(println)
+    val (c, p) = loadSchema(schema)
+    val m = initModel(c)
     val reader = new CSVReader(new FileReader(data), '*')
     val entries = reader.readAll
-    if (!entries.isEmpty) {
-      entries.toList.foreach(i => println(i.mkString(" --> ")))
+    for (e <- entries) {
+      println(e.mkString(" --> "))
     }
     reader.close
-
   }
 }
