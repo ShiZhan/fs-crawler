16fcd4b07e39e1f1cee085f36609278f082c74f3
ShiZhan <g.shizhan.g@gmail.com>
2013-11-09 00:25:42 -0600

    checksum collector script for Linux and Windows (Busybox)

--- test/checksum-collect.sh
+++ test/checksum-collect.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 # <directory> <output>
-find $1 -type f -print0 | xargs -0 md5sum -t | sed 's/  /*/' | awk 'BEGIN { FS = "*" }; "$0 !~ /^md5sum/" {print $2 "*" $1}' > $2
+#find $1 test/ -type f -exec md5sum -t {} \; | sed 's/  /*/' | awk 'BEGIN { FS = "*" }; "$0 !~ /^md5sum/" {print $2 "*" $1}' > $2
+#find $1 -type f -print0 | xargs -0 md5sum -t | sed 's/  /*/' | awk 'BEGIN { FS = "*" }; "$0 !~ /^md5sum/" {print $2 "*" $1}' > $2
+find $1 -type f -print0 | xargs -0 md5sum -t | sed 's/  /*/' | awk -v prefix=$2 'BEGIN { FS = "*" }; "$0 !~ /^md5sum/" {print prefix $2 "*" $1}' > $3
