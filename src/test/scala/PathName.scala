206ac9d2549aecb42fff06b2ba5734285c64fd41
ShiZhan <g.shizhan.g@gmail.com>
2013-11-11 15:48:59 -0600

    UNC path conversion

--- src/test/scala/PathName.scala
+++ src/test/scala/PathName.scala
@@ -1,26 +1,11 @@
-/**
- *
- */
-package util
+object PathName {
+  import java.io.File
+  import scala.util.Properties.{ envOrElse, userDir }
 
-import java.io.File
-import scala.util.Properties.{ envOrElse, userDir }
-
-/**
- * @author ShiZhan
- * configuration information
- * TGMROOT: TriGraM program root
- * TGMDATA: TriGraM program data
- * CIMDATA: CIM Schema Vocabulary and Model
- *
- * NOTE:
- * if above variables are not defined/exported, current directory will be used.
- */
-object Config {
   def UNC(fileName: String) = {
     val f = new File(fileName)
     val uri = f.toURI.toString
-    if (Platform.isWindows)
+    if (System.getProperty("os.name").startsWith("Windows"))
       f.toURI.toString.replaceFirst("file:/", "")
     else
       f.toURI.toString.replaceFirst("file:", "")
@@ -31,4 +16,11 @@ object Config {
   val TGMROOT = UNC(_TGMROOT)
   val TGMDATA = UNC(envOrElse("TGM_DATA", _PWD)) + ".trigram"
   val CIMDATA = UNC(envOrElse("CIM_DATA", _TGMROOT)) + "cim/"
+
+  def main(args: Array[String]) = {
+    println("PWD:  " + _PWD)
+    println("ROOT: " + TGMROOT)
+    println("DATA: " + TGMDATA)
+    println("CIM:  " + CIMDATA)
+  }
 }
