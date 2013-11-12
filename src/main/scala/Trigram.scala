33bc480f840e153751b41d459b5fb4ad1951f09f
ShiZhan <g.shizhan.g@gmail.com>
2013-11-12 13:29:39 -0600

    unify CLI

--- src/main/scala/Trigram.scala
+++ src/main/scala/Trigram.scala
@@ -8,19 +8,37 @@ object Trigram {
   import tdb.tdbquery.{ main => query }
   import tdb.tdbupdate.{ main => update }
   import console.Console
-  import util.Config.TGMDATA
-  import util.Version
+  import modeler.{ Modelers, CimVocabulary, Merger }
+  import util.{ Config, Version }
 
   val usage = """
-usage: Trigram [-h] [-v] [-i] [-q] [-u]
+usage: Trigram
  -h,--help                print this message
  -v,--version             show program version
+
+ console:
+ <no argument>            enter console
+
+ database:
  -i,--import MODEL        import model
  -q,--query SPARQL        http://www.w3.org/TR/sparql11-query/
  -u,--update SPARQL       http://www.w3.org/TR/sparql11-update/
 
- no argument              enter console
-"""
+ model cookers:
+ -c,--combine <MODEL...>         Combine multiple models
+ -V,--vocabulary CIM_Schema_XML  Update CIM vocabulary for use in modelers
+                                 [CIM Schema XML] can be downloaded from DMTF:
+                                 http://dmtf.org/standards/cim
+ -g,--gather MODEL               Gather CIM imports into given model
+                                 Based on the <OWL.imports> in [MODEL].
+
+ modeler:
+ -m,--modeler MODELER <args...>  Use [modeler] with arguments (below)
+                                 Each [modeler] has its own arguments.
+
+""" + Modelers.help
+
+  val incorrectArgs = "Incorrect parameters, see help (Translator -h, --help)."
 
   type OptionMap = Map[Symbol, Any]
 
@@ -36,65 +54,6 @@ usage: Trigram [-h] [-v] [-i] [-q] [-u]
       case "--query" :: q :: tail => Map('query -> q)
       case "-u" :: u :: tail => Map('update -> u)
       case "--update" :: u :: tail => Map('update -> u)
-      case _ => println("Incorrect option: " + list); sys.exit(1)
-    }
-  }
-
-  def main(args: Array[String]) = {
-    println("Triple Graph based Metadata storage - TriGraM")
-
-    if (args.length == 0) Console.run
-    else {
-      val options = parseOption(args.toList)
-
-      if (options.contains('help)) println(usage)
-      else if (options.contains('version)) println(Version.get)
-      else if (options.contains('model)) {
-        val modelList = options('model).asInstanceOf[List[String]]
-        modelList.foreach(loader(s"--loc=$TGMDATA", _))
-      } else if (options.contains('query)) {
-        val queryFile = options('query).toString
-        query(s"--loc=$TGMDATA", "--query=" + queryFile)
-      } else if (options.contains('update)) {
-        val updateFile = options('update).toString
-        update(s"--loc=$TGMDATA", "--update=" + updateFile)
-      }
-    }
-  }
-}
-
-object TrigramTranslator {
-  import modeler.{ Modelers, CimVocabulary, Merger }
-  import util.{ Version, Config }
-
-  val usage = s"""
-usage: Translator [flags] <arguments>
- -h,--help      Print this message
- -v,--version   Show program version
-
- build-in utilities:
- -c,--combine <MODEL...>         Combine multiple models
- -V,--vocabulary CIM_Schema_XML  Update CIM vocabulary for use in modelers
-                                 [CIM Schema XML] can be downloaded from DMTF:
-                                 http://dmtf.org/standards/cim
- -g,--gather MODEL               Gather CIM imports into given model
-                                 Based on the <OWL.imports> in [MODEL].
- -m,--modeler MODELER <args...>  Use [modeler] with arguments (below)
-                                 Each [modeler] has its own arguments.
-
- supported modelers:
-""" + Modelers.help
-
-  val incorrectArgs = "Incorrect parameters, see help (Translator -h, --help)."
-
-  type OptionMap = Map[Symbol, Any]
-
-  def parseOption(list: List[String]) = {
-    list match {
-      case "-h" :: tail => Map('help -> true)
-      case "--help" :: tail => Map('help -> true)
-      case "-v" :: tail => Map('version -> true)
-      case "--version" :: tail => Map('version -> true)
       case "-c" :: models => Map('combine -> models)
       case "--combine" :: models => Map('combine -> models)
       case "-V" :: cimSchema :: tail => Map('schema -> cimSchema)
@@ -110,15 +69,27 @@ usage: Translator [flags] <arguments>
   }
 
   def main(args: Array[String]) = {
-    println("TriGraM metadata translator")
+    println("Triple Graph based Metadata storage - TriGraM")
 
-    val options = parseOption(args.toList)
-    if (options.isEmpty)
-      println(incorrectArgs)
+    if (args.length == 0) Console.run
     else {
-      if (options.contains('help)) println(usage)
+      val tgmData = Config.TGMDATA
+      val cimData = Config.CIMDATA
+      val options = parseOption(args.toList)
+
+      if (options.isEmpty) println(incorrectArgs)
+      else if (options.contains('help)) println(usage)
       else if (options.contains('version)) println(Version.get)
-      else if (options.contains('combine)) {
+      else if (options.contains('model)) {
+        val modelList = options('model).asInstanceOf[List[String]]
+        modelList.foreach(loader(s"--loc=$tgmData", _))
+      } else if (options.contains('query)) {
+        val queryFile = options('query).toString
+        query(s"--loc=$tgmData", "--query=" + queryFile)
+      } else if (options.contains('update)) {
+        val updateFile = options('update).toString
+        update(s"--loc=$tgmData", "--update=" + updateFile)
+      } else if (options.contains('combine)) {
         val modelFiles = options('combine).asInstanceOf[List[String]]
         if (modelFiles.size > 1) {
           Merger.combine(modelFiles)
@@ -127,7 +98,7 @@ usage: Translator [flags] <arguments>
       } else if (options.contains('schema)) {
         val cimSchema = options('schema).toString
         CimVocabulary.generator(cimSchema)
-        println("CIM Vocabulary in [%s] are updated.".format(Config.CIMDATA))
+        println("CIM Vocabulary in [%s] are updated.".format(cimData))
       } else if (options.contains('gather)) {
         val baseModel = options('gather).toString
         Merger.gather(baseModel)
--- trigram.cmd
+++ trigram.cmd
@@ -1,17 +1,16 @@
 @REM launcher script
 @REM Shi.Zhan @ 2013
-@REM use 'sbt copy-dependencies' to collect dependencies
-@REM use absolute directory to load compiled classes
+@REM use 'sbt copy-dep' to collect dependencies in "target\scala-2.10\lib"
+@REM load compiled classes in "target\scala-2.10" directly for development
 @echo off
 setlocal
-@REM for querying big models
-set JAVA_OPTS=%JAVA_OPTS% -Xmx1g
 
+set JAVA_OPTS=%JAVA_OPTS% -Xmx1g
 set CP=
 set TGM_ROOT=%~dp0
 @REM set TGM_DATA=r:/data
 for /f %%i in ('dir /b %TGM_ROOT%target\scala-2.10\lib') do call :concat %%i
-scala -cp "%CP%;%TGM_ROOT%;%TGM_ROOT%target/scala-2.10/classes" Trigram %1 %2 %3 %4 %5 %6 
+scala -cp "%CP%;%TGM_ROOT%;%TGM_ROOT%target/scala-2.10/classes" Trigram %1 %2 %3 %4 %5 %6 %7 %8 %9
 endlocal
 goto :eof
 
