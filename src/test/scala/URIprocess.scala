85296e865d592e2f130336bd7b911f911218850f
ShiZhan <g.shizhan.g@gmail.com>
2013-11-13 00:38:56 -0600

    push to remote server for testing

--- src/test/scala/URIprocess.scala
+++ src/test/scala/URIprocess.scala
@@ -1,24 +1,34 @@
 object URIprocess {
   import org.apache.http.client.utils.URIBuilder
+  import com.hp.hpl.jena.util.FileUtils
+  import util.Platform.isWindows
+
+  def fileName2URI(fileName: String) = {
+    val n = FileUtils.toURL(fileName)
+    if (fileName.head == '/' && isWindows) n.drop(10) else n.drop(7)
+  }
+
+  val str1 = "r:\\Internet 临时文件\\Low\\Content.IE5\\XZC7FD6F\\T179zzFnhXXXXO75vn-225-90[1].jpg"
+  val str2 = "/home/user/foo/测试 空格/test"
+  val str3 = "r:\\TEMP/home/user/foo/测试 空格/test"
 
   def main(args: Array[String]) = {
-    val str = "r:\\Internet 临时文件\\Low\\Content.IE5\\XZC7FD6F\\T179zzFnhXXXXO75vn-225-90[1].jpg"
-    val str1 = str.replace('\\', '/')
-    val str2 = "/home/user/foo/测试 空格/test"
-    val str3 = "r:\\TEMP/home/user/foo/测试 空格/test"
+    println(str1)
+    println(str2)
+    println(str3)
+
     val ub = new URIBuilder
     ub.setHost(java.net.InetAddress.getLocalHost.getHostName)
     ub.setScheme("trigram")
     ub.setPath(str1)
-    println(str1)
     println(ub)
     ub.setPath(str2)
-    println(str2)
     println(ub)
-    val str4 = str3.replace('\\', '/')
-    ub.setPath(str4)
-    println(str3)
-    println(str4)
+    ub.setPath(str3)
     println(ub)
+
+    println(fileName2URI(str1))
+    println(fileName2URI(str2))
+    println(fileName2URI(str3))
   }
 }
