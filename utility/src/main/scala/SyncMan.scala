049304d2ca5bf1ca3bee5862f785e07da230c3ab
ShiZhan <g.shizhan.g@gmail.com>
2013-11-30 04:40:59 -0600

    test OK

--- utility/src/main/scala/SyncMan.scala
+++ utility/src/main/scala/SyncMan.scala
@@ -8,39 +8,24 @@ object SyncMan {
     val md5 = MessageDigest.getInstance("MD5")
     try {
       bytes.foreach(md5.update)
-    }
-    catch {
+    } catch {
       case e: Exception => throw e
     }
     md5.digest.map("%02x".format(_)).mkString
   }
 
-//  def md5(bytes: Iterator[Byte]) = {
-//    val blkLen = 65536
-//    val blocks = bytes.grouped(blkLen).map(_.toArray)
-//    val md5 = MessageDigest.getInstance("MD5")
-//    try {
-//      blocks.foreach(md5.update)
-//    }
-//    catch {
-//      case e: Exception => throw e
-//    }
-//    md5.digest.map("%02x".format(_)).mkString
-//  }
+  //  def md5(bytes: Iterator[Byte]) = {
+  //    val md5 = MessageDigest.getInstance("MD5")
+  //    try {
+  //      md5.update(bytes.toArray)
+  //    }
+  //    catch {
+  //      case e: Exception => throw e
+  //    }
+  //    md5.digest.map("%02x".format(_)).mkString
+  //  }
 
-//  def md5(bytes: Iterator[Byte]) = {
-//    val md5 = MessageDigest.getInstance("MD5")
-//    try {
-//      md5.update(bytes.toArray)
-//    }
-//    catch {
-//      case e: Exception => throw e
-//    }
-//    md5.digest.map("%02x".format(_)).mkString
-//  }
-
-  def fileMD5(fileName: String) = {
-    val file = new File(fileName)
+  def fileMD5(file: File) = {
     val fileBuffer = Source.fromFile(file, "ISO-8859-1")
     val fileBytes = fileBuffer.map(_.toByte)
     val md5sum = md5(fileBytes)
@@ -48,8 +33,7 @@ object SyncMan {
     md5sum
   }
 
-  def chunkMD5(fileName: String, chunkSize: Int) = {
-    val file = new File(fileName)
+  def chunkMD5(file: File, chunkSize: Int) = {
     val fileBuffer = Source.fromFile(file, "ISO-8859-1")
     val chunks = fileBuffer.grouped(chunkSize).map(_.map(_.toByte).iterator)
     val md5sumList = chunks.map { md5 }.toList
@@ -61,11 +45,35 @@ object SyncMan {
     if (args.length < 2)
       println("usage: SyncMan <directory> <chunk size>")
     else {
-      val md5 = fileMD5(args(0))
-      val md5List = chunkMD5(args(0), args(1).toInt).zipWithIndex
-      println("MD5: " + md5)
-      println("MD5 of chunks:")
-      md5List.foreach(println)
+      val dir = new File(args(0))
+      if (!dir.exists)
+        println("input source does not exist")
+      else if (dir.isFile)
+        println("input source is file")
+      else {
+        val chunkSize = args(1).toInt
+        val dirName = dir.getName
+        val sumFile = new PrintWriter(dirName + "-sum.csv")
+ 
+        def checkDir(d: File): Unit = {
+          val items = d.listFiles
+          val (files, dirs) = items.partition(_.isFile)
+          for (f <- files) {
+            val path = f.getAbsolutePath
+            val md5 = fileMD5(f)
+            val md5List =
+              if (f.length > chunkSize) chunkMD5(f, chunkSize).zipWithIndex else Nil
+  
+            sumFile.println(md5 + ";" + path)
+            md5List.foreach { case (m, i) => sumFile.println(m + ";" + path + "." + i) }
+          }
+          dirs.foreach(checkDir)
+        }
+
+        checkDir(dir)
+
+        sumFile.close
+      }
     }
   }
 
