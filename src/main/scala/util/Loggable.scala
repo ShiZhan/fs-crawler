a6b493cc95c14556720edf71457f52fae5d5444c
ShiZhan <g.shizhan.g@gmail.com>
2014-01-02 03:52:56 -0600

    use slf4j directly

--- src/main/scala/util/Loggable.scala
+++ src/main/scala/util/Loggable.scala
@@ -3,28 +3,10 @@
  */
 package util
 
-import org.slf4j.LoggerFactory
-
 /**
  * @author ShiZhan
  * for use slf4j with object
- * Note: implementation taken from scalax.logging API
- * https://github.com/eengbrec/Scalax.IO/blob/master/src/scalax/logging/log.scala
  */
 trait Logging {
-  val logger = Logging.getLogger(this)
-}
-
-object Logging {
-
-  def loggerNameForClass(className: String) =
-    if (className endsWith "$")
-      className.substring(0, className.length - 1)
-    else
-      className
-
-  def getLogger(logging: AnyRef) =
-    LoggerFactory.getLogger(
-      loggerNameForClass(logging.getClass.getName))
-
+  val logger = org.slf4j.LoggerFactory.getLogger(this.getClass)
 }
