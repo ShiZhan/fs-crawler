e3ae52fa38940f8043a22d5e1b9dcc4729575472
ShiZhan <g.shizhan.g@gmail.com>
2014-01-09 02:40:17 -0600

    code reuse

--- src/main/scala/modeler/Archive.scala
+++ src/main/scala/modeler/Archive.scala
@@ -12,7 +12,7 @@ object ArchiveModels {
   import com.hp.hpl.jena.rdf.model.{ ModelFactory, Model, Resource }
   import com.hp.hpl.jena.vocabulary.{ OWL, OWL2, DC_11 => DC, RDF }
   import com.hp.hpl.jena.datatypes.xsd.XSDDatatype._
-  import org.apache.commons.compress.archivers.ArchiveEntry
+  import helper.ArchiveCheckers.ArcEntryChecksum
   import helper.{ Logging, Version, DateTime, URI }
   import modeler.{ CimVocabulary => CIM }
 
@@ -47,10 +47,11 @@ object ArchiveModels {
     }
   }
 
-  case class ArcEntryModel(e: ArchiveEntry, checksum: String) {
-    def addTo(m: Model, zip: Resource, prefix: String) = {
+  implicit class ArcEntryModel(archiveEntryChecksum: ArcEntryChecksum) {
+    val ArcEntryChecksum(e, arcivePath, checksum) = archiveEntryChecksum
+    def addTo(m: Model, zip: Resource) = {
       val path = e.getName
-      val uri = URI.fromString(prefix + path)
+      val uri = URI.fromString(arcivePath + '/' + path)
       val size = e.getSize.toString
       val modi = DateTime.get(e.getLastModifiedDate)
       val cimClass = CIM.CLASS(if (e.isDirectory) "CIM_Directory" else "CIM_DataFile")
@@ -69,140 +70,19 @@ object ArchiveModels {
   }
 }
 
-object ArchiveCheckers {
-  import java.io.{ File, FileInputStream }
-  import org.apache.commons.compress.archivers.zip.ZipFile
-  import org.apache.commons.compress.archivers.tar.TarArchiveInputStream
-  import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream
-  import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream
-  import org.apache.commons.codec.digest.DigestUtils.md5Hex
-  import helper.DigestUtilsAddon.md5HexChunk
-  import ArchiveModels._
-
-  private def checkZip(file: File) = {
-    try {
-      val zf = new ZipFile(file)
-      val entries = zf.getEntries
-      val files = Iterator.continually {
-        if (entries.hasMoreElements) entries.nextElement else null
-      }.takeWhile(null !=).filter(!_.isDirectory)
-      files map { e =>
-        val is = zf.getInputStream(e)
-        val md5 = md5Hex(is)
-        is.close
-        ArcEntryModel(e, md5)
-      }
-    } catch {
-      case e: Exception => e.printStackTrace; Iterator[ArcEntryModel]()
-    }
-  }
-
-  private def checkGzip(file: File) = {
-    val fis = new FileInputStream(file)
-    val gzis = new GzipCompressorInputStream(fis)
-    val tis = new TarArchiveInputStream(gzis)
-    try {
-      val files = Iterator.continually(tis.getNextTarEntry)
-        .takeWhile(null !=).filter(_.isFile)
-      files map { e =>
-        val size = e.getSize
-        val md5 = md5HexChunk(tis, size)
-        ArcEntryModel(e, md5)
-      }
-    } catch {
-      case e: Exception => e.printStackTrace; Iterator[ArcEntryModel]()
-    }
-  }
-
-  private def checkBz2(file: File) = {
-    val fis = new FileInputStream(file)
-    val bzis = new BZip2CompressorInputStream(fis)
-    val tis = new TarArchiveInputStream(bzis)
-    try {
-      val files = Iterator.continually(tis.getNextTarEntry)
-        .takeWhile(null !=).filter(_.isFile)
-      files map { e =>
-        val size = e.getSize
-        val md5 = md5HexChunk(tis, size)
-        ArcEntryModel(e, md5)
-      }
-    } catch {
-      case e: Exception => e.printStackTrace; Iterator[ArcEntryModel]()
-    }
-  }
-
-  private def check7Zip(file: File) = {
-    import org.apache.commons.compress.archivers.sevenz.SevenZFile
-    import org.apache.commons.codec.digest.DigestUtils.getDigest
-    import org.apache.commons.codec.binary.Hex.encodeHexString
-
-    val STREAM_BUFFER_LENGTH = 1024 * 64
-    def md5Hex7Zip(data: SevenZFile, size: Long) = {
-      val MD = getDigest("MD5")
-      var buffer = new Array[Byte](STREAM_BUFFER_LENGTH)
-      var total = 0
-      val goal = size.toInt
-      while (total < goal) {
-        val read = data.read(buffer, 0, (goal - total) min STREAM_BUFFER_LENGTH)
-        total += read
-        MD.update(buffer, 0, read)
-      }
-      encodeHexString(MD.digest)
-    }
-
-    try {
-      val zf = new SevenZFile(file)
-      val entries = Iterator.continually { zf.getNextEntry }
-        .takeWhile(null !=).filter(!_.isDirectory)
-      entries map { e =>
-        val size = e.getSize
-        val md5 = md5Hex7Zip(zf, size)
-        ArcEntryModel(e, md5)
-      }
-    } catch {
-      case e: Exception => e.printStackTrace; Iterator[ArcEntryModel]()
-    }
-  }
-
-  type arcChecker = (File => Iterator[ArcEntryModel])
-  private val arcCheckers = Map[String, arcChecker](
-    "zip" -> checkZip,
-    "jar" -> checkZip,
-    "war" -> checkZip,
-    "apk" -> checkZip,
-    "epub" -> checkZip,
-    "odt" -> checkZip,
-    "ods" -> checkZip,
-    "odp" -> checkZip,
-    "odg" -> checkZip,
-    "docx" -> checkZip,
-    "xlsx" -> checkZip,
-    "pptx" -> checkZip,
-    "tgz" -> checkGzip,
-    "gz" -> checkGzip,
-    "bz2" -> checkBz2,
-    "7z" -> check7Zip)
-  private def defaultChecker(f: File) = Iterator[ArcEntryModel]()
-  private val exts = arcCheckers map { case (k, c) => k } toSet
-  def isKnownArchive(file: File) =
-    (exts contains file.getName.split('.').last) & file.isFile
-  def checkArc(file: File) = {
-    val fileNameExtension = file.getName.split('.').last
-    arcCheckers.getOrElse(fileNameExtension, defaultChecker _)(file)
-  }
-}
-
 object Archive extends Modeler with helper.Logging {
   import java.io.{ File, FileOutputStream }
   import ArchiveModels._
+  import helper.ArchiveCheckers._
   import helper.URI
   import helper.FileEx.FileOps
 
   override val key = "arc"
+
   override val usage =
     "<source> <output.owl> => [output.owl], now support [zip, gzip, bzip, 7z]."
 
-  def run(options: Array[String]) = {
+  def run(options: Array[String]) =
     options.toList match {
       case fileName :: output :: Nil => {
         val file = new File(fileName)
@@ -210,16 +90,15 @@ object Archive extends Modeler with helper.Logging {
       }
       case _ => logger.error("parameter error: [{}]", options)
     }
-  }
 
   private def translate(file: File, output: String) = {
     val arcPath = file.getAbsolutePath
     logger.info("Model all supported archive file in [{}]", arcPath)
     val m = ArcModel(URI.fromHost, key).create
     file.flatten.foreach { f =>
-      if (ArchiveCheckers.isKnownArchive(f)) {
+      if (isKnownArchive(f)) {
         val knownArchive = ArcFileModel(f).addTo(m)
-        ArchiveCheckers.checkArc(f) foreach { _.addTo(m, knownArchive, arcPath + '/') }
+        checkArc(f) foreach { _.addTo(m, knownArchive) }
       }
     }
     m.write(new FileOutputStream(output), "RDF/XML-ABBREV")
