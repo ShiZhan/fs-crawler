55b2ee8dcc549fb6910ae50204aca042e0946604
ShiZhan <g.shizhan.g@gmail.com>
2014-01-20 15:37:01 -0600

    style

--- src/main/scala/kernel/Merger.scala
+++ src/main/scala/kernel/Merger.scala
@@ -3,40 +3,31 @@
  */
 package kernel
 
-import cim.{Vocabulary => CIM}
-import common.ModelEx.ModelFileOps
-import common.ModelEx.ModelOps
-import common.ModelEx.ModelSeqOps
-import common.ModelEx.load
-import modeler.Modelers
-import scala.collection.JavaConversions.asScalaIterator
-
 /**
  * @author ShiZhan
  * 1. CIM Model Merger
  * 2. Generic model combiner
  */
 object Merger {
-  import scala.collection.JavaConversions._
-  import cim.{ Vocabulary => CIM }
+  import scala.collection.JavaConversions.asScalaIterator
+  import cim.Vocabulary.{ FFN, PURL2FN, isCimURI }
   import common.ModelEx._
+  import modeler.Modelers
 
   private def readCimImports(modelFile: String): List[String] = {
     val m = load(modelFile)
     val importFiles = m.getImports
-      .map { _.getObject.toString }
-      .filter(CIM.isCimURI)
-      .map(CIM.PURL2FN).toList
+      .map { _.getObject.toString }.filter(isCimURI).map(PURL2FN).toList
     m.close
     if (importFiles.isEmpty) List()
     else importFiles ::: importFiles.flatMap(readCimImports)
   }
 
   def gather(output: String, selected: List[String]) = {
-    val fns = { if (Nil == selected) Modelers.tbox.toList else selected }.map(CIM.FFN)
-    val all = { fns.flatMap(readCimImports) ++ fns }.distinct
+    val files = { if (Nil == selected) Modelers.tbox.toList else selected }.map(FFN)
+    val all = { files.flatMap(readCimImports) ++ files }.distinct
     println("[%d] CIM classes to gather:".format(all.length))
-    all foreach println
+    all.foreach(println)
     all.asModels.join.rebase.store(output)
   }
 
