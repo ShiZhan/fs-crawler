ed61bd7537388449958150b7a38b5fd85bd1fb5e
ShiZhan <g.shizhan.g@gmail.com>
2014-01-26 00:20:26 -0600

    BUG FIX: N-Triple load

--- src/main/scala/common/ModelEx.scala
+++ src/main/scala/common/ModelEx.scala
@@ -6,55 +6,37 @@ package common
 /**
  * @author ShiZhan
  * Model extended operation
- * 1. load:  use file input stream API for better compatibility
- *           load with base
- * 2. join:  combine all models in Seq
- *           combine all models in Seq to base model
- * 3. load:  load model from given file name list
- *           load model from given file name list with absolute base URI
- * 4. write: encapsulate API for model writing in given format
- *           with given file name or instance as "UTF-8"
+ * 1. load:      use file input stream API for better compatibility
+ * 2. join:      combine all models in Seq
+ *               combine all models in Seq to base model
+ * 3. asModels:  load model from given file name list
+ * 4. write:     encapsulate API for model writing in given format
+ *               with given file name or instance as "UTF-8"
  */
 object ModelEx extends helper.Logging {
+  import java.io.File
   import scala.collection.JavaConversions._
-  import java.io.{ File, FileOutputStream }
   import com.hp.hpl.jena.rdf.model.{ ModelFactory, Model }
-  import com.hp.hpl.jena.vocabulary.{ DC_11 => DC, OWL, RDF }
-  import com.hp.hpl.jena.datatypes.xsd.XSDDatatype._
   import com.hp.hpl.jena.util.FileManager
-  import FileEx.FileOps
-  import helper.{ Version, DateTime }
+  import FileEx._
 
   def createDefaultModel = ModelFactory.createDefaultModel
 
   def load(fileName: String) = {
-    val m = ModelFactory.createDefaultModel
     try {
-      val is = FileManager.get.open(fileName)
-      m.read(is, "")
+      FileManager.get.loadModel(fileName, "N3")
     } catch {
-      case e: Exception => logger.error(fileName + ": " + e.toString); m
-    }
-  }
-
-  def load(fileName: String, base: String) = {
-    val m = ModelFactory.createDefaultModel
-    try {
-      val is = FileManager.get.open(fileName)
-      m.read(is, base)
-    } catch {
-      case e: Exception => logger.error(fileName + ": " + e.toString); m
+      case e: Exception => logger.error(fileName + ": " + e.toString); createDefaultModel
     }
   }
 
   implicit class ModelFileOps(fileNames: Seq[String]) {
-    def asModels = fileNames map load
-    def asModels(base: String) = fileNames map { load(_, base) }
+    def asModels = fileNames.map(load)
   }
 
   implicit class ModelOps(m: Model) {
     def store(fileName: String) = {
-      val fos = new File(fileName).getWriter("UTF-8")
+      val fos = fileName.toFile.getWriter("UTF-8")
       m.write(fos)
       fos.close
       logger.info("[{}] triples written to [{}]", m.size, fileName)
@@ -68,7 +50,7 @@ object ModelEx extends helper.Logging {
     }
 
     def store(fileName: String, format: String) = {
-      val fos = new File(fileName).getWriter("UTF-8")
+      val fos = fileName.toFile.getWriter("UTF-8")
       m.write(fos, format)
       fos.close
       logger.info("[{}] triples written to [{}]", m.size, fileName)
@@ -83,11 +65,7 @@ object ModelEx extends helper.Logging {
   }
 
   implicit class ModelSeqOps(models: Seq[Model]) {
-    def join = {
-      val baseModel = ModelFactory.createDefaultModel
-      (baseModel /: models) { (r, m) => r union m }
-    }
-
+    def join = (createDefaultModel /: models) { (r, m) => r union m }
     def join(baseModel: Model) = (baseModel /: models) { (r, m) => r union m }
   }
 }
