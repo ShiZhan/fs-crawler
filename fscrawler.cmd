7409e2170bd7ee8ef075698082cf050b5fb403c4
ShiZhan <g.shizhan.g@gmail.com>
2014-04-18 13:42:12 -0500

    use sbt package

--- fscrawler.cmd
+++ fscrawler.cmd
@@ -1,8 +1,7 @@
 @REM launcher script
 @REM Shi.Zhan @ 2013
-@REM use 'sbt copy-dep' to collect dependencies in "target\scala-2.10\lib"
-@REM load compiled classes from "target\scala-2.10" directly for development
-@REM if deploy with individual "jar" libs, update "set LIB" to lib directory.
+@REM use 'sbt copyDep' to collect dependencies in "target\scala-2.10\lib"
+@REM load packaged JAR from "target\scala-2.10/fscrawler_2.10-1.0.jar"
 @echo off
 setlocal
 
@@ -12,7 +11,7 @@ set FSC_ROOT=%~dp0
 @REM set FSC_DATA=r:/data
 set LIB=%FSC_ROOT%target\scala-2.10\lib\
 for /f %%i in ('dir /b %LIB%') do call :concat %%i
-scala -cp "%CP%;%FSC_ROOT%;%FSC_ROOT%target/scala-2.10/classes" FSCrawler %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -cp "%CP%;%FSC_ROOT%target/scala-2.10/fscrawler_2.10-1.0.jar" FSCrawler %1 %2 %3 %4 %5 %6 %7 %8 %9
 endlocal
 goto :eof
 
