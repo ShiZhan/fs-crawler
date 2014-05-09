18e69eba9cec4e33821201f13229e3e767d6ec73
ShiZhan <g.shizhan.g@gmail.com>
2014-05-08 23:31:37 -0500

    code format

--- src/main/scala/common/DigestUtilsAddon.scala
+++ src/main/scala/common/DigestUtilsAddon.scala
@@ -3,10 +3,6 @@
  */
 package common
 
-import java.io.InputStream
-import org.apache.commons.codec.binary.Hex.encodeHexString
-import org.apache.commons.codec.digest.DigestUtils.getDigest
-
 /**
  * @author ShiZhan
  * Apache commons-codec DigestUtils Addon
